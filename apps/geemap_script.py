73acceaaa18bc1aa9f8f17663b86c355db3dddc8
Martanti <martantiaji@gmail.com>
2022-04-10 21:34:36 +0700

    Update geemap_script.py

--- apps/geemap_script.py
+++ apps/geemap_script.py
@@ -23,61 +23,47 @@ def clhT2():
     Map = geemap.Map()
     
     polygon = ee.Geometry.Polygon([
-        [[121.731876,-2.330221], [121.069735, -2.317823], [121.214026,-2.994612], [121.785511,-2.992766]]
+        [121.731876,-2.330221], [121.069735, -2.317823], [121.214026,-2.994612], [121.785511,-2.992766]
     ])
     
     studyarea= polygon
     
-    #Read the Data
-    col= ee.ImageCollection("LANDSAT/LC08/C01/T2_SR") \
-    .filterBounds(studyarea)
-    
-    Map.addLayer(col,{},"Natural Color")
-    print(col)
-    
     #Make a time
-    startYear = 2016
+    startYear = 2013
     endYear = 2020
-    
     startdate=ee.Date.fromYMD(startYear,1,1)
     enddate=ee.Date.fromYMD(endYear+1,12,31)
-    year_list = ee.List.sequence(startYear, endYear)
-    
-    #cloud masking area
-    def maskL8sr(col):
-        # Bits 3 and 5 are cloud shadow and cloud, respectively.
-        cloudShadowBitMask = (1 << 3)
-        cloudsBitMask = (1 << 5)
-        # Get the pixel QA band.
-        qa = col.select('pixel_qa')
-        # Both flags should be set to zero, indicating clear conditions.
-        mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0) \
-        .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
-        return col.divide(10000).divide(3.141593).updateMask(mask)
-    
+    yearlist = range(startYear, endYear)
+    print(yearlist)
+    #Read the Data
+    for col in yearlist:
+        col= ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")\
+            .filterBounds(studyarea).filter(ee.Filter.eq('year', yearlist)).first()
+        
+        #cloud masking area
+        def maskL8sr(col):
+            # Bits 3 and 5 are cloud shadow and cloud, respectively.
+            cloudShadowBitMask = (1 << 3)
+            cloudsBitMask = (1 << 5)
+            # Get the pixel QA band.
+            qa = col.select('pixel_qa')
+            # Both flags should be set to zero, indicating clear conditions.
+            mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0)\
+                .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
+            return col.divide(10000).divide(3.141593).updateMask(mask)
+        
     #Make a calculate for Clorophil-a
-    def clh_collection (ynz):
+    def chla (ynz) :
         image = col.filter(ee.Filter.calendarRange(ynz, ynz, 'year')).map(maskL8sr).mean()
         ndwi = image.normalizedDifference(['B3', 'B5']).rename('NDWI')
         clh_a = image.expression(
             'exp(-0.9889*((RrsB4)/(RrsB5))+0.3619)',
             {'RrsB4': image.select('B4'),
              'RrsB5': image.select('B5')}).updateMask(ndwi)
-        
-        return clh_a.set('year', ynz).set('month', 1)\
-    .set('date', ee.Date.fromYMD(ynz,1,1))\
-    .set('system:time_start',ee.Date.fromYMD(ynz,1,1))
-    
-    def clh():
-        return ee.ImageCollection.fromImages(year_list.flatten().map(fung_chl))
-    print(clh_collection, 'Clorophil-a')
-    
+        return clh_a.set('year', ynz).set('month', 1).set('date', ee.Date.fromYMD(ynz,1,1)).set('system:time_start',ee.Date.fromYMD(ynz,1,1)).map()
     parameter = {'min':0, 'max':1, 'palette':['blue','green']}
-    #range(i=2016, i<=2020, i++)
-    tahun= range (2016, 2021)
-    for i in tahun:
-        def clh (clh_collection):
-            chl = clh_collection.filter(ee.Filter.eq('year', i)).first().clip(studyarea)
-            Map.addLayer(clh,parameter,"Clorophyl-a"+i)
-     
-    Map.to_streamlit(height=700, width=1000)
+    clhcollection = ee.ImageCollection.fromImages([chla]).flatten()
+    
+    Map.addLayer(clhcollection, parameter, 'Clorophyll-a')
+    Map.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
+    Map
