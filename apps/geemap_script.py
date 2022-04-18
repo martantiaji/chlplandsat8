5bc1ada9a8c00cb2db9021cc6bf79831a35bc45a
Martanti <martantiaji@gmail.com>
2022-04-19 06:42:24 +0700

    Update geemap_script.py

--- apps/geemap_script.py
+++ apps/geemap_script.py
@@ -4,22 +4,21 @@ import streamlit as st
 import numpy as np
 
 
-def introduction():
-    st.title("Chlorophyll-a")
-    "#streamlit geemap klorofil-a"
+st.title("Chlorophyll-a")
+"#streamlit geemap klorofil-a"
 
-    st.markdown("""
-    
-    Aplikasi Web ini dibuat dengan menggunakan Streamlit untuk menampilkan nilai 
-    estimasi besar klorofil-a pada Danau Matano dan Danau Towuti menggunakan 
-    algoritma Jaelani 2015 berdasarkan jurnal [Pemetaan Distribusi Spasial Konsentrasi Klorofil-A dengan Landsat 8 di Danau Matano dan Danau Towuti, Sulawesi Selatan](http://lipi.go.id/publikasi/pemetaan-distribusi-spasial-konsentrasi-klorofil-a-dengan-landsat-8-di-danau-matano-dan-danau-towuti-sulawesi-selatan/2062)
-    
-    """)
+st.markdown("""
+
+Aplikasi Web ini dibuat dengan menggunakan Streamlit untuk menampilkan nilai 
+estimasi besar klorofil-a pada Danau Matano dan Danau Towuti menggunakan 
+algoritma Jaelani 2015 berdasarkan jurnal [Pemetaan Distribusi Spasial Konsentrasi Klorofil-A dengan Landsat 8 di Danau Matano dan Danau Towuti, Sulawesi Selatan](http://lipi.go.id/publikasi/pemetaan-distribusi-spasial-konsentrasi-klorofil-a-dengan-landsat-8-di-danau-matano-dan-danau-towuti-sulawesi-selatan/2062)
+
+""")
     
     
 def L8_T1():
     
-    st.header("Landsat 8 Surface Reflectance Tier 2")
+    st.header("Landsat 8 Surface Reflectance Tier 1")
     
     m = geemap.Map()
     
@@ -58,7 +57,7 @@ def L8_T1():
             .normalizedDifference(['B3', 'B5']) \
             .rename('NDWI')
         clorophil_a = image \
-            .expression('exp(-0.9889*((RrsB4)/(RrsB5))+0.3619)', {
+            .expression('10**(-0.9889*((RrsB4)/(RrsB5))+0.3619)', {
                 'RrsB4': image.select('B4'),
                 'RrsB5': image.select('B5')
             }) \
@@ -76,10 +75,125 @@ def L8_T1():
     print(clorophil_a_collection.getInfo())
 
     parameter = {'min':0, 'max':1, 'palette':['blue','green']}
-    m.addLayer(clorophil_a_collection,parameter,"Clorophyll-a")
-    m.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
-    m.to_streamlit(width=width, height=height)
+    #m.addLayer(clorophil_a_collection,parameter,"Clorophyll-a")
+    #m.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
+    #m.add_colorbar(
+    #parameter,
+    #label="Clorophyll-a (mg/m3)",
+    #orientation="horizontal",
+    #layer_name="Clorophyll-a",
+    #transparent_bg=True,
+    #)
+    #m.to_streamlit(width=width, height=height)
+    
+    with row1_col2:
+        selected_year = st.multiselect("Select a year", years)
+        add_chart = st.checkbox("Show chart")
 
+    if selected_year:
+        for year in selected_year:
+            Map.addLayer(calculate_clorophil_a(year), parameter, "Clorophyll-a " + year)
+
+        if add_chart:
+            Map.add_legend(
+                legend_title="Clorophyll-a (mg/m3)", builtin_legend="NLCD"
+            )
+        with row1_col1:
+            Map.to_streamlit(width=width, height=height)
+
+    else:
+        with row1_col1:
+            Map.to_streamlit(width=width, height=height)
+
+def L8_T2():
+    
+    st.header("Landsat 8 Surface Reflectance Tier 1")
+    
+    m = geemap.Map()
+    
+    start_year = 2013
+    end_year = 2020
+    study_area = ee.Geometry.Polygon([
+        [121.731876,-2.330221], [121.069735, -2.317823], [121.214026,-2.994612], [121.785511,-2.992766]
+    ])
+    
+    collection = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR") \
+        .filterBounds(study_area)
+    
+    yearlist = range(start_year, end_year)
+    
+    def mask_clouds(image):
+        # Bits 3 and 5 are cloud shadow and cloud, respectively.
+        cloud_shadow_bit_mask = (1 << 10)
+        clouds_bit_mask = (1 << 15)
+        # Get the pixel QA band.
+        qa = image.select('pixel_qa')
+        # Both flags should be set to zero, indicating clear conditions.
+        mask = qa.bitwiseAnd(cloud_shadow_bit_mask).eq(0) \
+            .And(qa.bitwiseAnd(clouds_bit_mask).eq(0))
+        return image \
+            .divide(10000) \
+            .divide(3.141593) \
+            .updateMask(mask)
+
+
+    def calculate_clorophil_a(year) :
+        image = collection \
+            .filter(ee.Filter.calendarRange(year, year, 'year')) \
+            .map(mask_clouds) \
+            .mean()
+        ndwi = image \
+            .normalizedDifference(['B3', 'B5']) \
+            .rename('NDWI')
+        clorophil_a = image \
+            .expression('10**(-0.9889*((RrsB4)/(RrsB5))+0.3619)', {
+                'RrsB4': image.select('B4'),
+                'RrsB5': image.select('B5')
+            }) \
+            .updateMask(ndwi)
+        return clorophil_a \
+            .set('year', year) \
+            .set('month', 1) \
+            .set('date', ee.Date.fromYMD(year,1,1)) \
+            .set('system:time_start',ee.Date.fromYMD(year, 1, 1))
+
+    clorophil_a_collection = ee.ImageCollection.fromImages([
+        calculate_clorophil_a(year)
+        for year in yearlist
+    ])
+    print(clorophil_a_collection.getInfo())
+
+    parameter = {'min':0, 'max':1, 'palette':['blue','green']}
+    #m.addLayer(clorophil_a_collection,parameter,"Clorophyll-a")
+    #m.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
+    #m.add_colorbar(
+    #parameter,
+    #label="Clorophyll-a (mg/m3)",
+    #orientation="horizontal",
+    #layer_name="Clorophyll-a",
+    #transparent_bg=True,
+    #)
+    #m.to_streamlit(width=width, height=height)
+    
+    with row1_col2:
+        selected_year = st.multiselect("Select a year", years)
+        add_chart = st.checkbox("Show chart")
+
+    if selected_year:
+        for year in selected_year:
+            Map.addLayer(calculate_clorophil_a(year), parameter, "Clorophyll-a " + year)
+
+        if add_chart:
+            Map.add_legend(
+                legend_title="Clorophyll-a (mg/m3)", builtin_legend="NLCD"
+            )
+        with row1_col1:
+            Map.to_streamlit(width=width, height=height)
+
+    else:
+        with row1_col1:
+            Map.to_streamlit(width=width, height=height)
+    
 def app():
     apps = ["Landsat 8 Surface Reflectance Tier 1", "Landsat 8 Surface Reflectance Tier 2"]
     
