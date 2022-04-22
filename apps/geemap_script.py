1868b79af4ca38a435bacf29f8cb36b29c6e5a7c
Martanti <martantiaji@gmail.com>
2022-04-23 02:05:25 +0700

    Update geemap_script.py

--- apps/geemap_script.py
+++ apps/geemap_script.py
@@ -3,7 +3,7 @@ import geemap
 import streamlit as st
 import numpy as np
 
-ee.initialize()
+ee.initialize(None)
 ee.Authenticate(authorization_code=None, quiet=None, code_verifier=None, auth_mode=None)
     
 def L8_T1():
