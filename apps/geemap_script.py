5ef06cb1c0d6feb9b700fea7f5416f5d5e70fedf
Martanti <martantiaji@gmail.com>
2022-04-23 02:09:15 +0700

    Update geemap_script.py

--- apps/geemap_script.py
+++ apps/geemap_script.py
@@ -3,9 +3,9 @@ import geemap
 import streamlit as st
 import numpy as np
 
-ee.Initialize(None)
 ee.Authenticate(authorization_code=None, quiet=None, code_verifier=None, auth_mode=None)
-    
+ee.initialize()
+
 def L8_T1():
     
     st.header("Landsat 8 Surface Reflectance Tier 1")
