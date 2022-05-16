4722805e6598588a492a28323ed78de2a80bb2f2
Martanti <martantiaji@gmail.com>
2022-05-16 13:10:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
-# sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
-# sudo apt-get update
-# sudo apt-get install python3-dev
-# sudo apt-get install gdal-bin
-# sudo apt-get install libgdal-dev
-# export CPLUS_INCLUDE_PATH=/usr/include/gdal
-# export C_INCLUDE_PATH=/usr/include/gdal
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install python3-dev
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+sudo pip install google-api-python-client
+sudo pip install pyCrypto
+sudo pip install earthengine-api
+python -c "import ee; ee.Initialize()"
+earthengine authenticate
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
 # gdal-config --version
 # pip install GDAL==$(gdal-config --version | awk -F'[.]' '{print $1"."$2}') localtileserver
 
